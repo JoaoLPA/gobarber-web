4bf389df708994a61d548ff9e9663865d97c3cd4
Joao Alves <joaol.alves@gmail.com>
2020-05-25 22:50:24 -0300

    input styles done

--- src/pages/Signup/signup.component.tsx
+++ src/pages/Signup/signup.component.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErors';
 
 import Input from '../../components/Input/input.component';
 import Button from '../../components/Button/button.component';
@@ -11,8 +13,11 @@ import { Container, Content, Background } from './signup.styles';
 import logo from '../../assets/logo.svg';
 
 const Signup: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Digite o seu nome'),
         email: Yup.string()
@@ -24,7 +29,8 @@ const Signup: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -33,7 +39,7 @@ const Signup: React.FC = () => {
       <Background />
       <Content>
         <img src={logo} alt="" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça o seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
---
+++ src/utils/getValidationErors.ts
@@ -0,0 +1,27 @@
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+// const getValidationErrors = (err: ValidationError): Errors => {
+//   const validationErrors: Errors = {};
+
+//   err.inner.forEach(error => {
+//     validationErrors[error.path] = error.message;
+//   });
+
+//   return validationErrors;
+// };
+
+// export default getValidationErrors;
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+
+  return validationErrors;
+}
